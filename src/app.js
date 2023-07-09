02abb76cde29bc10519393652a1b6425426e50d6
Pedro Nascimento <sn.jaypee@gmail.com>
2023-07-09 04:22:07 -0300

    adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -4,13 +4,18 @@ import cors from "cors";
 import accountsRouter from "./routes/accounts.routes.js";
 import registersRouter from "./routes/registers.routes.js";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = Express();
 app.use(cors());
 app.use(json());
 app.use(accountsRouter);
 app.use(registersRouter);
 
-app.listen(5000, () => {
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
     console.log("Listening on 5000");
 })
 
